615136b8a969cd0d706c1153ad914958cea4597f
オールドDQN <olddqn@icloud.com>
2026-01-12 19:27:33 +0900

    Create test_x.py

---
+++ test_x.py
@@ -0,0 +1,7 @@
+- name: Run Test
+        env:
+          X_API_KEY: ${{ secrets.X_API_KEY }}
+          X_API_SECRET: ${{ secrets.X_API_SECRET }}
+          X_ACCESS_TOKEN: ${{ secrets.X_ACCESS_TOKEN }}
+          X_ACCESS_SECRET: ${{ secrets.X_ACCESS_SECRET }}
+        run: python test_x.py  # ここを一時的に test_x.py にする
